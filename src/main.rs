fa2e62b259fa901525a0a872a9ef5a7924c269a6
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-10 09:41:03 +0430

    add: handle connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{prelude::*, BufRead},
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
@@ -10,6 +10,17 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
