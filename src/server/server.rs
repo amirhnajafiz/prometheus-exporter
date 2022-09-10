2837a852d6e6d95f6f899a5a2bbc2a11a671f9b2
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-10 10:05:17 +0430

    bug: in thread pool

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,11 +1,12 @@
 pub fn start(address: String) {
     // creating a TCP listener
     let listener = TcpListener::bind(address).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
