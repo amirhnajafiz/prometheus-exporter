f6ce7c5d59003eb7686e3126ab889200db19f795
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-12 10:54:06 +0430

    init: thread pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-pub struct ThreadPool;
+pub struct ThreadPool{}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+    {
     }
 }
