1a99a64ba294ddea9fdf1c3173b810a216f8dfb8
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-01 12:22:49 +0330

    update: exporter

--- exporter.py
+++ exporter.py
@@ -1,1 +1,51 @@
-print("Prometheus Exporter")
+import os
+import time
+from prometheus_client import start_http_server, Gauge, Enum
+import requests
+
+
+
+class AppMetrics:
+    def __init__(self, app_port=80, polling_interval_seconds=5):
+        self.app_port = app_port
+        self.polling_interval_seconds = polling_interval_seconds
+
+        # Prometheus metrics to collect
+        self.current_requests = Gauge("app_requests_current", "Current requests")
+        self.pending_requests = Gauge("app_requests_pending", "Pending requests")
+        self.total_uptime = Gauge("app_uptime", "Uptime")
+        self.health = Enum("app_health", "Health", states=["healthy", "unhealthy"])
+
+    def run(self):
+        while True:
+            self.fetch()
+            time.sleep(self.polling_interval_seconds)
+
+    def fetch(self):
+        resp = requests.get(url=f"http://localhost:{self.app_port}/status")
+        status_data = resp.json()
+
+        # Update Prometheus metrics with application metrics
+        self.current_requests.set(status_data["current_requests"])
+        self.pending_requests.set(status_data["pending_requests"])
+        self.total_uptime.set(status_data["total_uptime"])
+        self.health.state(status_data["health"])
+
+
+def main():
+    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "5"))
+    app_port = int(os.getenv("APP_PORT", "80"))
+    exporter_port = int(os.getenv("EXPORTER_PORT", "9877"))
+
+    app_metrics = AppMetrics(
+        app_port=app_port,
+        polling_interval_seconds=polling_interval_seconds
+    )
+
+    start_http_server(exporter_port)
+    app_metrics.run()
+
+
+
+if __name__ == "__main__":
+    main()
