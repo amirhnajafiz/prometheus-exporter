915864146538430a2c7af9662532a5ae79c2557d
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-03 14:52:27 +0330

    add: metrics

--- app/main.py
+++ app/main.py
@@ -1,22 +1,29 @@
 import fastapi
 from fastapi import FastAPI
+from datetime import datetime
 
 
 
 # create an app instance
 app = FastAPI()
 
+# metrics
+requests = 0
+pendings = 0
+start_time = datetime.now()
+
 
 @app.get("/")
 def root():
+    requests = requests + 1
     return {"Version": fastapi.__version__}
 
 
 @app.get("/status")
 def status():
     return {
-        "current_requests": "",
-        "pending_requests": "",
-        "total_uptime": "",
+        "current_requests": requests,
+        "pending_requests": pendings,
+        "total_uptime": datetime.now()-start_time,
         "health": "healthy"
     }
