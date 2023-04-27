71d885a62356f27ada00a16596a0dfbc755e9e4e
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-27 16:12:09 +0330

    add: app host

--- metrics.py
+++ metrics.py
@@ -5,23 +5,31 @@ import requests
 
 
 class AppMetrics:
-    def __init__(self, app_port=80, polling_interval_seconds=5):
+    """
+    AppMetrics is the metrics class in where
+    we create our prometheus metrics to collect.
+    """
+    def __init__(self, app_host="127.0.0.1", app_port=80, polling_interval_seconds=5):
+        self.app_host = app_host
         self.app_port = app_port
         self.polling_interval_seconds = polling_interval_seconds
 
-        # Prometheus metrics to collect
+        # metrics
         self.current_requests = Gauge("app_requests_current", "Current requests")
         self.pending_requests = Gauge("app_requests_pending", "Pending requests")
         self.total_uptime = Gauge("app_uptime", "Uptime")
         self.health = Enum("app_health", "Health", states=["healthy", "unhealthy"])
 
     def run(self):
+        """
+        in run method, we pull the metrics in an interval time.
+        """
         while True:
             self.fetch()
             time.sleep(self.polling_interval_seconds)
 
     def fetch(self):
-        resp = requests.get(url=f"http://localhost:{self.app_port}/status")
+        resp = requests.get(url=f"http://{self.app_host}:{self.app_port}/status")
         status_data = resp.json()
 
         # Update Prometheus metrics with application metrics
